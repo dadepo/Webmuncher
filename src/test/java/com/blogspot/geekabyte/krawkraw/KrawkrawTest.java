65417c6fdd104e3736c254216fa1a03c04c32ab3
Dadepo Aderemi <dadepo@gmail.com>
2014-12-30 16:28:49 +0100

    Added regression test for https://github.com/dadepo/Krawkraw/issues/1 : ascertain behaviour with broken links

--- src/test/java/com/blogspot/geekabyte/krawkraw/KrawkrawTest.java
+++ src/test/java/com/blogspot/geekabyte/krawkraw/KrawkrawTest.java
@@ -4,12 +4,9 @@ import com.blogspot.geekabyte.krawkraw.interfaces.KrawlerAction;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,13 +14,11 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Future;
@@ -38,27 +33,7 @@ public class KrawkrawTest {
     private final String host = "http://localhost:" + TestServer.HTTP_PORT;
 
     TestServer testServer;
-
-    @Test
-    public void test_extractAbsHref() {
-        KrawlerAction defaultMockAction = mock(KrawlerAction.class);
-        Krawkraw krawkrawSUT = new Krawkraw(defaultMockAction);
-        Document doc = createDocument("fivelinks");
-        // System under test
-        List<String> hrefs = extractHref(doc);
-        assertEquals(hrefs.size(), 5);
-    }
-
-    @Test
-    public void test_extractHref() {
-        KrawlerAction defaultMockAction = mock(KrawlerAction.class);
-        Krawkraw krawkrawSUT = new Krawkraw(defaultMockAction);
-        Document doc = createDocument("fivelinks");
-        // System under test
-        List<String> hrefs = extractHref(doc);
-        assertEquals(hrefs.size(), 5);
-    }
-
+    
     @Test
     public void test_extractAllFromUrl() throws Exception {
         KrawlerAction mockAction = mock(KrawlerAction.class);
@@ -98,31 +73,35 @@ public class KrawkrawTest {
 
         testServer.shutDown();
     }
+    
+    @Test
+    public void test_extractBrokenLink() throws Exception {
+        KrawlerAction mockAction = mock(KrawlerAction.class);
+        Krawkraw krawkrawSUT = new Krawkraw(mockAction);
 
+        testServer = new TestServer();
+        testServer.start();
 
-    //==================================================== Helpers ====================================================
+        krawkrawSUT.setDelay(0);
+        // System under test
+        Set<String> hrefs = krawkrawSUT.doKrawl(host + "/brokenlink/index.html");
 
-
-    private Document createDocument(String type) {
-
-        switch (type) {
-            case ("fivelinks"): {
-                URL path = this.getClass().getResource("/fivelinks.html");
-                File file = new File(path.getFile());
-                Document doc = null;
-                try {
-                    doc = Jsoup.parse(file, "UTF-8");
-                } catch (IOException e) {
-                    logger.error("Error with file {}", file);
-                }
-
-                return doc;
-            }
-            default: {
-                return null;
+        ArgumentCaptor<FetchedPage> captor = ArgumentCaptor.forClass(FetchedPage.class);
+        assertEquals(hrefs.size(), 5);
+        verify(mockAction, times(5)).execute(captor.capture());
+        List<FetchedPage> fetchedPages = captor.getAllValues();
+        
+        int notFoundCount = 0;
+        for (FetchedPage page: fetchedPages) {
+            if (page.getStatus() == 404) {
+                notFoundCount++;
             }
         }
+        assertEquals(notFoundCount, 3);
+        testServer.shutDown();
     }
+
+    //==================================================== Helpers ====================================================
 
     private class TestServer {
 
@@ -150,18 +129,27 @@ public class KrawkrawTest {
                     response.setContentType("text/html;charset=utf-8");
                     response.setStatus(HttpServletResponse.SC_OK);
                     baseRequest.setHandled(true);
-                    response.getWriter().println(getContent(target));
+                    String content = getContent(target);
+                    if (String.valueOf(HttpServletResponse.SC_NOT_FOUND).equals(content)) {
+                        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                    }
+                    response.getWriter().println(content);
                 }
 
                 private String getContent(String filename) {
                     byte[] contentAsBytes = null;
+                    String pathAsString;
                     try {
                         URL pathAsUrl = this.getClass().getResource(filename);
-                        String pathAsString = pathAsUrl.getPath();
-
+                        if (pathAsUrl == null) {
+                            pathAsString = "";
+                        } else {
+                            pathAsString = pathAsUrl.getPath();
+                        }
                         contentAsBytes = Files.readAllBytes(Paths.get(pathAsString));
                     } catch (IOException e) {
                         logger.error("Exception while reading {}", filename);
+                        return String.valueOf(HttpServletResponse.SC_NOT_FOUND);
                     }
                     if (contentAsBytes != null) {
                         return new String(contentAsBytes, StandardCharsets.UTF_8);
@@ -173,14 +161,4 @@ public class KrawkrawTest {
         }
     }
 
-    private List<String> extractHref(Document doc) {
-        List<String> hrefString = new ArrayList<String>();
-        Element content = doc.body();
-        Elements links = content.getElementsByTag("a");
-        for (Element link : links) {
-            String href = link.attr("href");
-            hrefString.add(href);
-        }
-        return hrefString;
-    }
 }
